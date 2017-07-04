04e150ea6ad09df0c27abd7706d6a4ea31401408
Joan Molinas Ramon <joanmramon@gmail.com>
2017-07-04 14:06:56 +0200

    [CORE] Auth models created

---
+++ Sources/HMPAuth.swift
@@ -0,0 +1,72 @@
+//
+//  HMPAuth.swift
+//  HampKit
+//
+//  Created by Joan Molinas Ramon on 4/7/17.
+//  Copyright © 2017 com.hamp. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+public typealias SuccessBlock = ((HMPUser) -> ())?
+public typealias ErrorBlock = ((Error) -> ())?
+
+public struct HMPAuth {
+    
+    /// Log in to firebase
+    ///
+    /// - Parameters:
+    ///   - mail: email to log in
+    ///   - password: password to log in
+    ///   - successBlock: block called if all was successfull
+    ///   - errorBlock: block called if error ocurred
+    public static func signIn(
+        mail : String,
+        password : String,
+        successBlock: SuccessBlock,
+        errorBlock: ErrorBlock) {
+        
+    }
+    
+    /// Sign up to firebase
+    ///
+    /// - Parameters:
+    ///   - mail: email to log in
+    ///   - password: password to log in
+    ///   - successBlock: block called if all was successfull
+    ///   - errorBlock: block called if error ocurred
+    public static func signUp(
+        mail : String,
+        password : String,
+        successBlock: SuccessBlock,
+        errorBlock: ErrorBlock) {
+        
+    }
+    
+    /// Sign out on firebase
+    ///
+    /// - Parameters:
+    ///   - success: block called if all was successfull
+    ///   - error: block called if error ocurred
+    public static func signOut(
+        success : SuccessBlock,
+        error : ErrorBlock) {
+        
+    }
+    
+    /// Log in to firebase
+    ///
+    /// - Parameters:
+    ///   - accessToken: token retrieved from facebook sdk
+    ///   - successBlock: block called if all was successfull
+    ///   - errorBlock: block called if error ocurred
+    public static func singInWithFacebook(
+        accessToken : String,
+        successBlock: SuccessBlock,
+        errorBlock: ErrorBlock) {
+        
+    }
+    
+    
+}
---
+++ Sources/HMPUser.swift
@@ -0,0 +1,15 @@
+//
+//  HMPUser.swift
+//  HampKit
+//
+//  Created by Joan Molinas Ramon on 4/7/17.
+//  Copyright © 2017 com.hamp. All rights reserved.
+//
+
+import Foundation
+
+public struct HMPUser {
+    //MARK: Properties
+    var uid : String
+    var email : String
+}
