7fd190e123c49f9745ad4293fab66bc3d5742299
Joan Molinas Ramon <joanmramon@gmail.com>
2017-07-05 21:59:42 +0200

    [CORE] HMPConstants

---
+++ Sources/HampConstants.swift
@@ -0,0 +1,23 @@
+//
+//  HampConstants.swift
+//  HampKit
+//
+//  Created by Joan Molinas Ramon on 5/7/17.
+//  Copyright © 2017 com.hamp. All rights reserved.
+//
+
+import Foundation
+
+internal struct Constants {
+    public struct FirebaseTableNames {
+        static let lockersTableName = "lockers"
+    }
+    
+    public struct Locker {
+        static let booked = "booked"
+        static let lockerID = "lockerID"
+        static let secretKey = "secretKey"
+    }
+    
+    
+}
