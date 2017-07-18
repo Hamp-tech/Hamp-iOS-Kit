3186240a80ba5ff24ad5ce9ea62830437466ee3b
Joan Molinas Ramon <joanmramon@gmail.com>
2017-07-18 16:06:20 +0200

    [CORE] HampAPIBooking requests

---
+++ Sources/HampAPIBooking.swift
@@ -0,0 +1,54 @@
+//
+//  HampAPIBooking.swift
+//  HampKit
+//
+//  Created by Joan Molinas Ramon on 18/7/17.
+//  Copyright © 2017 com.hamp. All rights reserved.
+//
+
+import Foundation
+
+public final class HampAPIBooking {
+    
+    ///Properties
+    static let path = Constants.ServerPaths.booking
+    
+    /// Booking a new locker to user
+    ///
+    /// - Parameters:
+    ///   - user: the user wants booking a locker
+    ///   - card: card to pay
+    ///   - order: order to booking
+    ///   - onSuccess: called if booking was successfully
+    ///   - onError: callef if an error occurred
+    static func booking(user: HampUser,
+                        card: HampCreditCard,
+                        order: HampOrder,
+                        onSuccess: ServerSuccess<HampBooking>,
+                        onError: ServerError) {
+        
+        guard let uid = user.identifier else {
+            preconditionFailure("User hasn't identifier")
+        }
+        
+        guard let cid = card.identifier else {
+            preconditionFailure("Card hasn't identifier")
+        }
+        
+        let bookingPath = String(format: path, uid)
+        
+        let transaction = try? HampTransaction(identifier: nil, userID: nil, cardID: cid, order: order)
+        let params = transaction?.propertiesDictionary()
+        
+        HampServerManager
+            .sharedManager
+            .request(HampBooking.self,
+                     path: bookingPath,
+                     method: .post,
+                     parameters: params,
+                     onSuccess: onSuccess,
+                     onError: onError)
+        
+    }
+    
+}
