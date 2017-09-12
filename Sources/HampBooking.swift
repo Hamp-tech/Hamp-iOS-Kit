3dfa620865143f35bc38dc13a80c0220551d6be7
Joan Molinas Ramon <joanmramon@gmail.com>
2017-09-12 10:04:17 +0200

    [CORE] HampBooking constructors and properties optionals instead of nonoptionals

--- Sources/HampBooking.swift
+++ Sources/HampBooking.swift
@@ -9,17 +9,28 @@
 import Foundation
 
 public struct HampBooking : HampObject {
+    
     //MARK: Properties
-    public var transaction : HampTransaction
-    public var delivery_locker : HampLocker
-    public var collection_locker : HampLocker
+    public var transaction : HampTransaction?
+    public var delivery_locker : HampLocker?
+    public var collection_locker : HampLocker?
+
+    //MARK: Constructors
+    init(transaction: HampTransaction?,
+         delivery_locker: HampLocker?,
+         collectionLocker: HampLocker?) {
+        self.transaction = transaction
+        self.delivery_locker = delivery_locker
+        self.collection_locker = collectionLocker
+    }
+    
     
     /// Validate
     ///
     /// - Throws: throw if transaction or locker is not valid
     public func validate() throws {
-        try transaction.validate()
-        try delivery_locker.validate()
-        try collection_locker.validate()
+        try transaction?.validate()
+        try delivery_locker?.validate()
+        try collection_locker?.validate()
     }
 }
